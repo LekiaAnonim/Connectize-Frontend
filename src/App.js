208d946478698a42e7f51b8909f61943719c7958
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-10-28 19:48:42 +0100

    modified App.js and signup.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 //import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import FeedPage from './pages/feedPages/feedPage';
-import Signup from './components/auth-forms/signup';
-import Login from './components/auth-forms/login';
-import NoPage from './components/NoPage';
-import Profile from './components/profile/profile';
-import Home from './components/profile/home';
-import Contact from './components/profile/contact';
-import Address from './components/profile/address';
-import Bio from './components/profile/bio';
-import SuccessPage from './components/auth-forms/successpage';
-import Overview from './components/profile/overview';
-import CompanyBioOne from './components/profile/companyBioOne';
-import CompanyBioTwo from './components/profile/companyBioTwo';
-import CompanyBioThree from './components/profile/companyBioThree';
-import CompanyBioFour from './components/profile/companyBioFour';
-import Services from './pages/service/service';
-import ServiceOverView from './pages/service/serviceOverview';
-import ServiceAdmin from './pages/service/serviceAdmin';
-import Market from './pages/market/market';
-import Product from './pages/market/product';
-import Analysis from './pages/market/analysis';
-import Listing from './pages/market/listing';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FeedPage from "./pages/feedPages/feedPage";
+import Signup from "./components/auth-forms/signup";
+import Login from "./components/auth-forms/login";
+import NoPage from "./components/NoPage";
+import Profile from "./components/profile/profile";
+import Home from "./components/profile/home";
+import Contact from "./components/profile/contact";
+import Address from "./components/profile/address";
+import Bio from "./components/profile/bio";
+import SuccessPage from "./components/auth-forms/successpage";
+import Overview from "./components/profile/overview";
+import CompanyBioOne from "./components/profile/companyBioOne";
+import CompanyBioTwo from "./components/profile/companyBioTwo";
+import CompanyBioThree from "./components/profile/companyBioThree";
+import CompanyBioFour from "./components/profile/companyBioFour";
+import Services from "./pages/service/service";
+import ServiceOverView from "./pages/service/serviceOverview";
+import ServiceAdmin from "./pages/service/serviceAdmin";
+import Market from "./pages/market/market";
+import Product from "./pages/market/product";
+import Analysis from "./pages/market/analysis";
+import Listing from "./pages/market/listing";
 import UserProfile from "./pages/feedPages/userProfile";
 import AdminProfile from "./pages/feedPages/adminProfile";
 import NewsFeed from "./pages/feedPages/newsFeed";
@@ -31,42 +31,47 @@ import Navbar from "./pages/feedPages/antway";
 function App() {
   return (
     <>
-    <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
-    </head>
-    <BrowserRouter>
-      <Routes>
-        <Route>
-          <Route index element={<NewsFeed/>} />
-          <Route path='signup' element={<Signup />} />
-          <Route path='login' element={<Login />} />
-          <Route path='successpage' element={<SuccessPage />} />
-          <Route path='profile' element={<Profile />}/>
-          <Route path='home' element={<Home />}/>
-          <Route path='contact' element={<Contact />}/>
-          <Route path='address' element={<Address />}/>
-          <Route path='bio' element={<Bio/>}/>
-          <Route path='overview' element={<Overview />}/>
-          <Route path='bioone' element={<CompanyBioOne />}/>
-          <Route path='biotwo' element={<CompanyBioTwo />}/>
-          <Route path='biothree' element={<CompanyBioThree />}/>
-          <Route path='biofour' element={<CompanyBioFour />}/>
-          <Route path='service' element={<Services/>}/>
-          <Route path='serviceoverview' element={<ServiceOverView/>}/>
-          <Route path='serviceadmin' element={<ServiceAdmin/>}/>
-          <Route path='market' element={<Market/>}/>
-          <Route path='product' element={<Product/>}/>
-          <Route path='analysis' element={<Analysis/>}/>
-          <Route path='listing' element={<Listing/>}/>
-          <Route path='userprofile' element={<UserProfile/>}/>
-          <Route path='adminprofile' element={<AdminProfile/>}/>
-          <Route path='feedpage' element={<FeedPage/>}/>
-          <Route path='testing' element={<Testing/>}/>
-          <Route path='antway' element={<Navbar/>}/>
-          <Route path='*' element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous"
+        ></link>
+      </head>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route index element={<NewsFeed />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+            <Route path="success" element={<SuccessPage />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="home" element={<Home />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="address" element={<Address />} />
+            <Route path="bio" element={<Bio />} />
+            <Route path="overview" element={<Overview />} />
+            <Route path="bioone" element={<CompanyBioOne />} />
+            <Route path="biotwo" element={<CompanyBioTwo />} />
+            <Route path="biothree" element={<CompanyBioThree />} />
+            <Route path="biofour" element={<CompanyBioFour />} />
+            <Route path="service" element={<Services />} />
+            <Route path="serviceoverview" element={<ServiceOverView />} />
+            <Route path="serviceadmin" element={<ServiceAdmin />} />
+            <Route path="market" element={<Market />} />
+            <Route path="product" element={<Product />} />
+            <Route path="analysis" element={<Analysis />} />
+            <Route path="listing" element={<Listing />} />
+            <Route path="userprofile" element={<UserProfile />} />
+            <Route path="adminprofile" element={<AdminProfile />} />
+            <Route path="feedpage" element={<FeedPage />} />
+            <Route path="testing" element={<Testing />} />
+            <Route path="antway" element={<Navbar />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/auth-forms/signup.js
+++ src/components/auth-forms/signup.js
@@ -1,101 +1,172 @@
-import React, { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import {Link, useNavigate } from "react-router-dom"
-import axios from 'axios';
-//import NavDropdown from 'react-bootstrap/NavDropdown';
-
-
-//const API_HOST = 'https://connectize.co/api/auth';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Signup() {
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [password1, setPassword1] = useState('')
-    const [password2, setPassword2] = useState('')
-    const navigate = useNavigate()
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const navigate = useNavigate();
 
-
-    //axios.defaults.withCredentials=true;
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post("https://connectize.co/api/auth/registration/", { username, email, password1, password2 })
-        .then(res=>{
-            if(res){
-                console.log(res)
-                alert('Registration Succesfull')
-                navigate('/successpage')
-            }
-        })
-        .catch(err=>console.log(err))
-    }
-    return (
-        <>
-            <div className='bg-white row'>
-                <div className='col-sm-12 col-md-6 col-lg-5 ms-5 mb-5'>
-                    <Navbar.Brand href="#home">
-                        <img src='/images/logo.png' style={{ height: "60px" }} alt='logo' />
-                    </Navbar.Brand>
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <h1 style={{ marginTop: "4%" }}>Create new account</h1><br />
-                            <div>
-                                <label htmlFor='username'>Username</label><br /><br />
-                                <input type='email' className='form-control' style={{ height: "50px" }} placeholder='Username@example.com' required
-                                    onChange={(e) => setUsername(e.target.value)} />
-                            </div><br />
-                            <div>
-                                <label>Company email</label><br /><br />
-                                <input type='text' className='form-control' style={{ height: "50px" }} placeholder='Company@example.com' required
-                                    onChange={(e) => setEmail(e.target.value)} />
-                            </div><br />
-                            <div>
-                                <label>Password</label><br /><br />
-                                <input type='password' className='form-control' style={{ height: "50px" }} placeholder='**********' required
-                                    onChange={(e) => setPassword1(e.target.value)} />
-                            </div><br />
-                            <div>
-                                <label>Confirm Password</label><br /><br />
-                                <input type='password' className='form-control' style={{ height: "50px" }} placeholder='**********' required
-                                    onChange={(e) => setPassword2(e.target.value)} />
-                            </div><br />
-                            <div>
-                                <input type='checkbox' /> I agree to <a><b>Terms and Condition</b></a>    
-                            </div><br />
-                            <div>
-                                <button type='submit' className='btn btn-dark w-75 rounded-pill' style={{ height: "50px" }}>Sign up</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div className='col-sm-12 col-md-6 col-lg-6 text-white'>
-                    <Navbar expand="md" className="bg-body-white">
-                        <Container>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="ms-auto me-4 text-white gap-4">
-                                    <Nav.Link href="/" className='text-white h6'>Home</Nav.Link>
-                                    <Nav.Link href="/service" className='text-white h6'>Service</Nav.Link>
-                                    <Nav.Link href="/about" className='text-white h6'>About</Nav.Link>
-                                    <Nav.Link href="/market" className='text-white h6'>Market</Nav.Link>
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
-                    <div style={{ marginTop: "-4rem" }}>
-                        <img src="./images/signup-image.png" style={{ width: "100%", height: "710px" }} alt='pics' />
-                    </div>
-                    <div className='d-flex' style={{ marginTop: "-30%", marginLeft: "10%" }}>
-                        <h2 >Already have an <br /> account?</h2>
-                        <Link to="/login" className='btn btn-warning rounded-pill h-25 w-25 ms-4'
-                        >Login</Link>
-                    </div>
-                </div>
+  //axios.defaults.withCredentials=true;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://connectize.co/api/auth/registration/", {
+        username,
+        email,
+        password1,
+        password2,
+      })
+      .then((res) => {
+        if (res) {
+          console.log(res);
+          alert("Registration Succesfull");
+          navigate("/success");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <>
+      <div className="bg-white row">
+        <div className="col-sm-12 col-md-6 col-lg-5 ms-5 mb-5">
+          <Navbar.Brand href="#home">
+            <img src="/images/logo.png" style={{ height: "60px" }} alt="logo" />
+          </Navbar.Brand>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <h1 style={{ marginTop: "4%" }}>Create new account</h1>
+              <br />
+              <div>
+                <label htmlFor="username">Username</label>
+                <br />
+                <br />
+                <input
+                  type="email"
+                  className="form-control"
+                  style={{ height: "50px" }}
+                  placeholder="Username@example.com"
+                  required
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <br />
+              <div>
+                <label>Company email</label>
+                <br />
+                <br />
+                <input
+                  type="text"
+                  className="form-control"
+                  style={{ height: "50px" }}
+                  placeholder="Company@example.com"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <br />
+              <div>
+                <label>Password</label>
+                <br />
+                <br />
+                <input
+                  type="password"
+                  className="form-control"
+                  style={{ height: "50px" }}
+                  placeholder="**********"
+                  required
+                  onChange={(e) => setPassword1(e.target.value)}
+                />
+              </div>
+              <br />
+              <div>
+                <label>Confirm Password</label>
+                <br />
+                <br />
+                <input
+                  type="password"
+                  className="form-control"
+                  style={{ height: "50px" }}
+                  placeholder="**********"
+                  required
+                  onChange={(e) => setPassword2(e.target.value)}
+                />
+              </div>
+              <br />
+              <div>
+                <input type="checkbox" /> I agree to{" "}
+                <a>
+                  <Link to="/">
+                    <b>Terms and Condition</b>
+                  </Link>
+                </a>
+              </div>
+              <br />
+              <div>
+                <button
+                  type="submit"
+                  className="btn btn-dark w-75 rounded-pill"
+                  style={{ height: "50px" }}
+                >
+                  Sign up
+                </button>
+              </div>
             </div>
-        </>
-    )
+          </form>
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-6 text-white">
+          <Navbar expand="md" className="bg-body-white">
+            <Container>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto me-4 text-white gap-4">
+                  <Nav.Link href="/" className="text-white h6">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link href="/service" className="text-white h6">
+                    Service
+                  </Nav.Link>
+                  <Nav.Link href="/about" className="text-white h6">
+                    About
+                  </Nav.Link>
+                  <Nav.Link href="/market" className="text-white h6">
+                    Market
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <div style={{ marginTop: "-4rem" }}>
+            <img
+              src="./images/signup-image.png"
+              style={{ width: "100%", height: "710px" }}
+              alt="pics"
+            />
+          </div>
+          <div
+            className="d-flex"
+            style={{ marginTop: "-30%", marginLeft: "10%" }}
+          >
+            <h2>
+              Already have an <br /> account?
+            </h2>
+            <Link
+              to="/login"
+              className="btn btn-warning rounded-pill h-25 w-25 ms-4"
+            >
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Signup
+export default Signup;
