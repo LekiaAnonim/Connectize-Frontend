1d5df6f833eba36a61cc72c2bac39dd51ca772cb
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-11-23 01:28:16 +0100

    modified notifications.js in components

--- src/components/notifications.js
+++ src/components/notifications.js
@@ -28,28 +28,31 @@ export function NotificationPopOver() {
 
 export function Notifications({ className }) {
   return (
-    <div className={clsx("bg-white rounded p-3 space-y-4 w-full", className)}>
+    <div
+      className={clsx(
+        "bg-white rounded p-3 space-y-4 w-full max-w-screen-xs min-w-[300px]",
+        className
+      )}
+    >
       <header className="flex justify-between items-center gap-2 border-b border-gray-100 pb-1">
         <h4 className="text-xl font-semibold">Notifications</h4>
-        <button className="text-gray-500 hover:text-black transition-colors duration-300 underline">
+        <button className="text-gray-500 hover:text-black transition-colors duration-300 hover:underline !text-xs">
           Clear All
         </button>
       </header>
       <div className="space-y-4">
         {notificationMessages.map(
           ({ company, src, message, timeStamp }, index) => (
-            <div key={index} className="flex items-center gap-2">
+            <div key={index} className="flex items-start gap-2">
               <img
                 src={src}
                 alt={company}
-                width={40}
-                height={40}
+                width={35}
+                height={35}
                 className="rounded-full"
               />
               <div className="space-y-0">
-                <h3 className="text-base leading-[1.125] font-bold m-0">
-                  {company}
-                </h3>
+                <h3 className="leading-[1.125] font-bold m-0">{company}</h3>
                 <p className="text-sm text-gray-700 line-clamp-1">{message}</p>
                 <small className="text-gray-400 text-xs m-0">{timeStamp}</small>
               </div>
