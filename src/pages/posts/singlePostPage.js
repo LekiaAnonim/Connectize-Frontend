cae01020e683483cf217aaffb1d82219f780fe1e
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-12-13 23:09:40 +0100

    modified singlePostPage.js in posts

--- src/pages/posts/singlePostPage.js
+++ src/pages/posts/singlePostPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useParams } from "react-router-dom";
 import { getPosts } from "../../api-services/posts";
 import { useQuery } from "@tanstack/react-query";
@@ -6,24 +6,32 @@ import { useCustomQuery } from "../../context/queryContext";
 import { DiscoverPostItem } from "../../components/admin/feeds/DiscoverPosts";
 
 function SinglePostPage() {
-  const params = useParams();
+  const { id } = useParams();
   const { refetchInterval } = useCustomQuery();
+
   const { data: posts, isLoading } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
     refetchInterval,
+    staleTime: 300000, // Reduce unnecessary fetching by making data stale after 5 minutes
+    cacheTime: 600000, // Cache data for 10 minutes to minimize re-fetching
   });
 
-  const postItem = posts?.find((post) => post.id.toString() === params.id);
+  // Use useMemo to avoid re-computation of the postItem on every render
+  const postItem = useMemo(
+    () => posts?.find((post) => post.id.toString() === id),
+    [posts, id]
+  );
 
-  if (isLoading) return <p>loading...</p>;
-  if (!postItem && !isLoading) return <p>No content found</p>;
+  if (isLoading) return <p>Loading...</p>;
+  if (!postItem) return <p>No content found</p>;
+
   return (
     <section className="space-y-4">
       <DiscoverPostItem
         postItem={postItem}
-        hasImage={postItem.images.length > 0}
-        isSinglePost={true}
+        hasImage={postItem.images?.length > 0}
+        isSinglePost
       />
     </section>
   );
