048a0743cd304ddb756b9bbee01d69f08851ef9f
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-01-03 14:48:52 +0100

    changed 74 files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.connectize.app',
+  appName: 'connectize',
+  webDir: 'build',
+};
+
+export default config;
