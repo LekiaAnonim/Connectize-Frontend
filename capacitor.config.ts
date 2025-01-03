a9f6da095c6b5c8fead5d86d28628661e533b12c
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-01-03 16:28:52 +0100

    modified workspace.xml and capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,16 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.connectize.app',
-  appName: 'connectize',
-  webDir: 'build',
+  appId: "com.connectize.app",
+  appName: "connectize",
+  webDir: "build",
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#000",
+    },
+  },
 };
 
 export default config;
