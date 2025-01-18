de922acf2e93a34912e8a728279075d602062a35
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-01-18 15:59:23 +0100

    modified checkAgreement.jsx in form

--- src/components/form/checkAgreement.jsx
+++ src/components/form/checkAgreement.jsx
@@ -6,7 +6,7 @@ export default function CheckAgreement({ formik }) {
     <>
       <div className="flex items-center space-x-2 my-1">
         <input
-          className="size-3.5 cursor-pointer"
+          className="size-3.5 cursor-pointer checked:!bg-gold che "
           name="isChecked"
           type="checkbox"
           onChange={formik.handleChange}
@@ -16,7 +16,7 @@ export default function CheckAgreement({ formik }) {
           htmlFor="terms"
         >
           I agree to{" "}
-          <Link className="text-black font-bold" to="/">
+          <Link className="!text-black font-bold" to="/">
             Terms and Conditions
           </Link>
         </label>
