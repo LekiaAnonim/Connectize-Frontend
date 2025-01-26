cd9e754dedcc35714e032c0d02bd76522f04db35
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-01-26 13:09:57 +0100

    modified index.js in helpers

--- src/lib/helpers/index.js
+++ src/lib/helpers/index.js
@@ -18,10 +18,10 @@ export function goToLogin() {
 }
 
 // Configure Axios Defaults
-export const baseURL = "https://connectize.co";
-// process.env.NODE_ENV === "development"
-//   ? "http://127.0.0.1:8000"
-//   : "https://connectize.co";
+export const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:8000"
+    : "https://connectize.co";
 axios.defaults.withCredentials = true;
 
 // Mutex for Refresh Token
