f010f1d142e2c083912307c2b0bc39ce4fd458a1
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-02-28 08:02:54 +0100

    modified ParagraphText.jsx and Content.jsx in components

--- src/pages/terms&policies/components/Content.jsx
+++ src/pages/terms&policies/components/Content.jsx
@@ -1,27 +1,32 @@
 import React from "react";
+import HeadingText from "../../../components/HeadingText";
 
-export default function Content({ array = [] }) {
+export default function Content({ array = [], activeSection }) {
   return (
     <section className="w-3/4 p-4">
-      {array.map((policy, index) => {
+      {array.map((item, index) => {
         return (
-          <div key={index} className="mb-8">
+          <section key={index} className="mb-8">
             <h2 className="text-2xl font-bold text-gray-800 mb-4">
-              {policy.title}
+              {item.title}
             </h2>
             <p className="text-sm text-gray-500 mb-4">
-              Last Updated: {policy.lastUpdated}
+              Last Updated: {item.lastUpdated}
             </p>
-            <p className="text-gray-600 mb-4">{policy.content}</p>
-            {policy.details.map((detail, idx) => (
-              <div key={idx} className="mb-4">
-                <h3 className="text-xl font-semibold text-gray-700">
+            <p className="text-gray-600 mb-4">{item.content}</p>
+            {item.details.map((detail, idx) => (
+              <div
+                key={idx}
+                className="mb-4"
+                id={detail.heading.replace(/\s+/g, "-").toLowerCase()}
+              >
+                <HeadingText heading="sub-heading">
                   {detail.heading}
-                </h3>
-                <p className="text-gray-600">{detail.text}</p>
+                </HeadingText>
+                <p className="text-gray-500">{detail.text}</p>
               </div>
             ))}
-          </div>
+          </section>
         );
       })}
     </section>
