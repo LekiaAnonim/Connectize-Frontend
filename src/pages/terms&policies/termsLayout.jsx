e404ec32dd0b4096045cd81ddb4f7243aa5ef685
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-02-28 07:53:10 +0100

    modified termsLayout.jsx in terms&policies

--- src/pages/terms&policies/termsLayout.jsx
+++ src/pages/terms&policies/termsLayout.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Logo from "../../components/logo";
 
 export default function TermsLayout() {
   return (
-      <main className="max-w-screen-lg mx-auto md:my-2 p-6 bg-white rounded-lg flex">
-          <Outlet />
+    <main className="md:space-y-4">
+      <header className="container max-w-screen-lg max-md:py-2">
+        <Logo />
+      </header>
+      <section className="container max-w-screen-lg p-6 bg-white rounded-lg flex">
+        <Outlet />
+      </section>
     </main>
   );
 }
